30582afed04e2483477b7be1e227e38332ee5a17
Invius <marco.fernandes.inf10@hotmail.com>
2023-03-11 19:51:59 +0000

    si adaptation

--- index.js
+++ index.js
@@ -15,6 +15,35 @@ const configAxios = {
   headers: commonHeaders,
 };
 
+const withBody = ['*', (req, res) => {
+  
+  try {
+    const payload =  JSON.parse(req.body || {});
+
+    configAxios.headers['store-origin'] = req.socket ? req.socket.remoteAddress : 'error';
+    const method = req.method.toLowerCase();
+    const isDelete = method === 'delete';
+    const params = isDelete ?  [{ data: payload, headers: configAxios.headers}] : [payload, configAxios];
+
+    console.log(req.method.toLowerCase(), axios[req.method.toLowerCase()], `${process.env.API_TO_REDIRECT}${req.url}`, params);
+    
+    axios[req.method.toLowerCase()](`${process.env.API_TO_REDIRECT}${req.url}`,
+    ...params)
+    .then( (response) => {
+      res.send(response.data);
+    }).catch((err)=>{
+      res.status(500);
+      res.send({error: "Internal Error"});
+    }).finally(()=>{
+      console.log('done');
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+    res.send({error: "Internal Error"});
+  }
+}]
+
 app.use(cors());
 
 
@@ -52,27 +81,10 @@ app.get("*", function (req, res) {
 
 });
 
-app.post("*", function (req, res) {
-  
-  try {
-    const payload =  JSON.parse(req.body || {});
-
-    configAxios.headers['store-origin'] = req.socket ? req.socket.remoteAddress : 'error';
-
-    axios.post(`${process.env.API_TO_REDIRECT}${req.url}`,
-    payload, configAxios)
-    .then( (response) => {
-      res.send(response.data);
-    }).catch((err)=>{
-      res.status(500);
-      res.send({error: "Internal Error"});
-    });
-  } catch (error) {
-    res.status(500);
-    res.send({error: "Internal Error"});
-  }
-});
+app.post(...withBody).put(...withBody).patch(...withBody).delete(...withBody);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+
